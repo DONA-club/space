b428a961d0185fb6f9c755be4a323882944e8f8c
SachaUlysse <jeoffretsacha@gmail.com>
2025-12-01 23:46:39 +0100

    [dyad] Demande des points d’entrée pour appliquer un lissage ±60 s au curseur - wrote 1 file(s)

--- src/store/appStore.ts
+++ src/store/appStore.ts
@@ -142,7 +142,7 @@ export const useAppStore = create<AppState>((set) => ({
   idwPower: 2,
   meshResolution: 40,
   visualizationType: 'points',
-  smoothingWindowSec: 0,
+  smoothingWindowSec: 120,
   interpolationRange: null,
   wsConnected: false,
   isRecording: false,
